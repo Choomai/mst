e2d2f389c2f7e02742891900ac1d29b712c11a95
Choomai <nguyencaonguyen0944322545@gmail.com>
2024-12-13 00:36:10 +0700

    oops, 1 only, the docs says "either"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.baolong.mst"
         minSdk = 26
         versionCode = 1
-        versionName = "0.2.0"
+        versionName = "0.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         targetSdk = 34
