1dfef98c355a7626319d5741e311d9ce06fa1ce1
Choomai <nguyencaonguyen0944322545@gmail.com>
2024-12-12 08:41:03 +0700

    added view model, copied from TaskViewModel

--- app/src/main/java/com/baolong/mst/Note.kt
+++ app/src/main/java/com/baolong/mst/Note.kt
@@ -1,6 +1,5 @@
 package com.baolong.mst
 
-import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
---
+++ app/src/main/java/com/baolong/mst/TimetableViewModel.kt
@@ -0,0 +1,39 @@
+package com.baolong.mst
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class TimetableViewModel(database: AppDatabase): ViewModel() {
+    val events = mutableStateOf<List<TimetableEvent>>(emptyList())
+    private val timetableDao = database.timetableDao()
+
+    init {
+        viewModelScope.launch(Dispatchers.IO) {
+            this@TimetableViewModel.events.value = timetableDao.getAllEvents()
+        }
+    }
+
+    fun insertTask(event: TimetableEvent) {
+        viewModelScope.launch(Dispatchers.IO) {
+            timetableDao.insertEvent(event)
+            this@TimetableViewModel.events.value = timetableDao.getAllEvents()
+        }
+    }
+
+    fun updateTask(event: TimetableEvent) {
+        viewModelScope.launch(Dispatchers.IO) {
+            timetableDao.updateEvent(event)
+            this@TimetableViewModel.events.value = timetableDao.getAllEvents()
+        }
+    }
+
+    fun deleteTask(event: TimetableEvent) {
+        viewModelScope.launch(Dispatchers.IO) {
+            timetableDao.deleteEvent(event)
+            this@TimetableViewModel.events.value = timetableDao.getAllEvents()
+        }
+    }
+}
